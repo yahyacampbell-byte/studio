411e8984b8794707ee30aa9749ff5744296e5aac
John Campbell <yahyacampbell@gmail.com>
2025-05-17 10:39:42 +0000

    Latest rollout failed with this error message in the build log: Error: Build process exited with error code 1.     at ChildProcess.<anonymous> (/layers/google.nodejs.firebasenextjs/npm_modules/node_modules/@apphosting/common/dist/index.js:36:24)     at ChildProcess.emit (node:events:524:28)     at ChildProcess._handle.onexit (node:internal/child_process:293:12) Node.js v20.19.2"} ERROR: failed to build: exit status 1 ERROR: failed to build: executing lifecycle: failed with status code: 51 Finished Step #3 - "pack" ERROR ERROR: build step 3 "gcr.io/k8s-skaffold/pack" failed: step exited with non-zero status: 1

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: false, // Changed to false
   },
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: false, // Changed to false
   },
   images: {
     remotePatterns: [
